3cf2c71a1a5874532e3b82eacaa9e9d49737b25a
Erdem ÖZCAN <rdmzcn@gmail.com>
2021-10-23 14:02:48 +0300

    added test in integration

--- Rover.Test/IntegrationTest.cs
+++ Rover.Test/IntegrationTest.cs
@@ -18,13 +18,21 @@ namespace Rover.Test
             var roverResults = routerService.Route(roverRouters).ToList();
             var expectedResults = testData.listRoverRouters.Select(t => t.expected).ToList();
 
-            Assert.Equal(expectedResults.First().Direction, roverResults.First().Direction);
-            Assert.Equal(expectedResults.First().Location.X, roverResults.First().Location.X);
-            Assert.Equal(expectedResults.First().Location.Y, roverResults.First().Location.Y);
+            Assert.Equal(expectedResults.ElementAt(0).Direction, roverResults.ElementAt(0).Direction);
+            Assert.Equal(expectedResults.ElementAt(0).Location.X, roverResults.ElementAt(0).Location.X);
+            Assert.Equal(expectedResults.ElementAt(0).Location.Y, roverResults.ElementAt(0).Location.Y);
 
-            Assert.Equal(expectedResults.Last().Direction, roverResults.Last().Direction);
-            Assert.Equal(expectedResults.Last().Location.X, roverResults.Last().Location.X);
-            Assert.Equal(expectedResults.Last().Location.Y, roverResults.Last().Location.Y);
+            Assert.Equal(expectedResults.ElementAt(1).Direction, roverResults.ElementAt(1).Direction);
+            Assert.Equal(expectedResults.ElementAt(1).Location.X, roverResults.ElementAt(1).Location.X);
+            Assert.Equal(expectedResults.ElementAt(1).Location.Y, roverResults.ElementAt(1).Location.Y);
+
+            Assert.Equal(expectedResults.ElementAt(2).Direction, roverResults.ElementAt(2).Direction);
+            Assert.Equal(expectedResults.ElementAt(2).Location.X, roverResults.ElementAt(2).Location.X);
+            Assert.Equal(expectedResults.ElementAt(2).Location.Y, roverResults.ElementAt(2).Location.Y);
+
+            Assert.Equal(expectedResults.ElementAt(3).Direction, roverResults.ElementAt(3).Direction);
+            Assert.Equal(expectedResults.ElementAt(3).Location.X, roverResults.ElementAt(3).Location.X);
+            Assert.Equal(expectedResults.ElementAt(3).Location.Y, roverResults.ElementAt(3).Location.Y);
         }
     }
 
@@ -44,7 +52,15 @@ namespace Rover.Test
                     Movement.MoveForward, Movement.MoveForward, Movement.TurnRight, Movement.MoveForward,
                     Movement.MoveForward, Movement.TurnRight, Movement.MoveForward, Movement.TurnRight,
                     Movement.TurnRight, Movement.MoveForward
-                }), new Rover(new Coordinate(5, 1), Direction.East))
+                }), new Rover(new Coordinate(5, 1), Direction.East)),
+                (new RoverRouter(new Rover(new Coordinate(0, 0), Direction.West), new List<Movement>()
+                {
+                    Movement.MoveForward, Movement.TurnLeft, Movement.MoveForward
+                }), new Rover(new Coordinate(0, 0), Direction.South)),
+                (new RoverRouter(new Rover(new Coordinate(5, 5), Direction.North), new List<Movement>()
+                {
+                    Movement.MoveForward, Movement.TurnRight, Movement.MoveForward
+                }), new Rover(new Coordinate(5, 5), Direction.East))
             }));
         }
     }
